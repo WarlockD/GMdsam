3d9e7f2fc37dc5c5f46f3014b40d0f5a267b4220
Paul Bruner <warlockd@gmail.com>
2016-07-16 09:12:24 -0500

    Made the events clearer and fixed bugs on them in objects

--- GMdsam/Writers/GameMaker/GM_Writer.cs
+++ GMdsam/Writers/GameMaker/GM_Writer.cs
@@ -15,24 +15,24 @@ namespace GameMaker.Writers.GameMaker
         public Writer(BlockToCode output) : base(output)
         {
         }
-        void WriteAction(int @event, ILBlock block)
+        void WriteAction(int @event, ActionInfo action)
         {
             string eventName = Constants.MakePrittyEventName(@event, 0);
-            output.WriteLine("Event: {0}", eventName);
-            output.Write(block); // auto ident
+            output.WriteLine("Event: {0}", action.Name);
+            output.Write(action.Method); // auto ident
             output.WriteLine();
         }
-        void WriteAction(int @event, int subEvent, ILBlock block)
+        void WriteAction(int @event, int subEvent, ActionInfo action)
         {
             string eventName = Constants.MakePrittyEventName(@event, subEvent);
-            output.WriteLine("Event: {0}",  eventName);
-            output.Write(block); // auto ident
+            output.WriteLine("Event: {0}", action.Name);
+            output.Write(action.Method); // auto ident
             output.WriteLine();
         }
         void WriteAction(int @event, List<ActionInfo> actions)
         {
             output.WriteLine("self.{0} = self.{0} or {{}}", Constants.LookUpEvent(@event));
-            foreach (var func in actions) WriteAction(@event, func.SubType, func.Method);
+            foreach (var func in actions) WriteAction(@event, func.SubType, func);
         }
     
        
@@ -71,17 +71,17 @@ namespace GameMaker.Writers.GameMaker
                 {
                     case 0:
                         Debug.Assert(e.Actions.Count == 1);
-                        WriteAction(0, e.Actions.Single().Method);
+                        WriteAction(0, e.Actions.Single());
                         break;
                     case 1:
                         Debug.Assert(e.Actions.Count == 1);
-                        WriteAction(1, e.Actions.Single().Method);
+                        WriteAction(1, e.Actions.Single());
                         break;
                     case 2:
                         WriteAction(2, e.Actions);
                         break;
                     case 3:
-                        foreach (var a in e.Actions) WriteAction(3, a.SubType, a.Method);
+                        foreach (var a in e.Actions) WriteAction(3, a.SubType, a);
                         break;
                     case 4:
                         WriteAction(4, e.Actions);
@@ -90,15 +90,15 @@ namespace GameMaker.Writers.GameMaker
                         WriteAction(5, e.Actions);
                         break;
                     case 6:
-                        foreach (var a in e.Actions) WriteAction(6, a.SubType, a.Method);
+                        foreach (var a in e.Actions) WriteAction(6, a.SubType, a);
                         break;
                     case 7: // we only really care about user events
-                        foreach (var a in e.Actions) WriteAction(7, a.SubType, a.Method);
+                        foreach (var a in e.Actions) WriteAction(7, a.SubType, a);
                         break;
                     case 8:
                         foreach (var a in e.Actions)
                         { // special case for 0 since I cannot find it
-                            WriteAction(7, a.SubType, a.Method);
+                            WriteAction(7, a.SubType, a);
                         }
                         break;
                     case 9:
